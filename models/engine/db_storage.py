5934046accca4d855bb1420d1038826746e2db25
Glitch (dfbq91-airbnb-clone-v2-1) <none>
2020-03-31 22:42:34 +0000

    🙈📼 Checkpoint ./models/engine/db_storage.py:13740128/138 ./models/state.py:13740095/671

---
+++ models/engine/db_storage.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""This is the DB storage class for AirBnB"""
+
+class DBStorage:
+    """This class manage the data base"""
--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is the state class"""
 import models
+import os
 from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, Integer, String
@@ -15,13 +16,18 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     
-    @property
-    def cities(self):
-        '''returns the list of City instances with
-        state_id equals to the current State.id'''
-        all_cities = models.storage.all(City)
-        cities_in_state = []
-        for city in all_cities.values():
-            if city.state_id == self.id:
-                cities_in_state.append(city)
-        return cities_in_state
+    if os.getenv('HBNB_TYPE_STORAGE') == 'file'
+        @property
+        def cities(self):
+            '''returns the list of City instances with
+            state_id equals to the current State.id'''
+            all_cities = models.storage.all(City)
+            cities_in_state = []
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    cities_in_state.append(city)
+            return cities_in_state
+    elif os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        def cities(self):
+          '''represent a relationship with the class City'''
+    
