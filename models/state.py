a256ffb8311cfe84cd819544badc5b238640d4b9
Glitch (dfbq91-airbnb-clone-v2-1) <none>
2020-03-31 23:54:34 +0000

    💕🏭 Checkpoint ./models/state.py:13740095/38

--- models/state.py
+++ models/state.py
@@ -16,7 +16,7 @@ class State(BaseModel, Base):
     
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete")
+    cities = relationship("City", cascade="all, delete", back_ref="state")
     
     if os.getenv('HBNB_TYPE_STORAGE') == 'file'
         @property
