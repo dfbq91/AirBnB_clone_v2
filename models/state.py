86842b5a50d2487c36a9e29d3661e883b20a5ae6
Glitch (dfbq91-airbnb-clone-v2-1) <none>
2020-03-31 22:18:32 +0000

    👋🏬 Checkpoint ./models/state.py:13740128/419

--- models/state.py
+++ models/state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This is the state class"""
+import models
 from models.base_model import BaseModel
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -14,6 +16,11 @@ class State(BaseModel, Base):
     
     @property
     def cities(self):
-      '''returns the list of City instances with
-      state_id equals to the current State.id'''
-      
+        '''returns the list of City instances with
+        state_id equals to the current State.id'''
+        all_cities = models.storage.all(City)
+        cities_in_state = []
+        for city in all_cities.values():
+            if city.state_id == self.id:
+                cityes_in_state.append(city)
+        return cities_in_
