9c63e3063b8f6fa05a570b3fcaedff7c4bd0efbd
dfbq91 <dfbq91@gmail.com>
2020-04-22 12:36:21 -0500

    flask web

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Starts a flask application'''
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.teardown_appcontext
+def remove_SQLAlchemy_session():
+    '''remove SQLAlchemy session'''
+    storage.close()
+
+
+@app.route('/states_list')
+def states():
+    '''displays states in html page'''
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
