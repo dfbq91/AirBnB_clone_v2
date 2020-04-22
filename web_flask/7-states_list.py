82cec835d7bffff4a4920d18e8b608140421f129
dfbq91 <dfbq91@gmail.com>
2020-04-22 19:15:06 +0000

    imported State class from models.state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,8 +2,10 @@
 '''Starts a flask application'''
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.teardown_appcontext
 def remove_SQLAlchemy_session(self):
